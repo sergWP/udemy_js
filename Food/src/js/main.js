cc98ac74dd8b37cec50a49792c02c29ba671fd94
sergWP <kuzmenko.sergei@gmail.com>
2021-11-27 10:28:53 +0200

    48 lesson (CLASS practic)

--- Food/src/js/main.js
+++ Food/src/js/main.js
@@ -152,4 +152,61 @@ window.addEventListener('DOMContentLoaded', () => {
 
     window.addEventListener('scroll', showModalByScroll);
 
+    // MENU - CLASS
+
+    class Menu {
+        constructor(image, name, description, price, selector) {
+            this.image = image;
+            this.name = name;
+            this.description = description;
+            this.parent = document.querySelector(selector);
+            this.price = price;
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+
+        changeToUAH() {
+            this.price = Math.ceil(this.price * this.transfer);
+        }
+
+        insertMenuCard() {
+            const element = document.createElement('div');
+            element.innerHTML = `<div class="menu__item">
+                                    <img src=${this.image} alt="${this.name}">
+                                    <h3 class="menu__item-subtitle">${this.name}</h3>
+                                    <div class="menu__item-descr">${this.description}</div>
+                                    <div class="menu__item-divider"></div>
+                                    <div class="menu__item-price">
+                                        <div class="menu__item-cost">Цена:</div>
+                                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                                    </div>
+                                </div>`;
+            this.parent.append(element);
+        }
+    }
+
+    new Menu(
+        'img/tabs/vegy.jpg',
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9.25,
+        '.menu .container'
+    ).insertMenuCard();
+
+    new Menu(
+        'img/tabs/elite.jpg',
+        'Меню "Премиум"',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        16.36,
+        '.menu .container'
+    ).insertMenuCard();
+
+    new Menu(
+        'img/tabs/post.jpg',
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        14.88,
+        '.menu .container'
+    ).insertMenuCard();
+
 });
