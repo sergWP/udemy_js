55a0b5cd981da222fe6d5789f8364a566fe95b53
sergWP <kuzmenko.sergei@gmail.com>
2021-12-26 18:22:22 +0200

    72 lesson (ES6 modules)

--- TEST/src/index.js
+++ TEST/src/index.js
@@ -1,7 +1,23 @@
 // IMPORT
-
 const myModule = require('./js/script_71');
 const myModuleInstance = new myModule();
 
 myModuleInstance.hello();
 myModuleInstance.bye();
+
+// ES6 import
+// импорт попереммнным
+import sayHi, {one, two} from './js/script';
+console.log(`${one} and ${two}`);
+
+// переименовывание
+import {one as first} from './js/script';
+console.log(`One as First: ${first}`);
+
+// импорт всего и определяем как data
+// import * as data from './js/script';
+// data.sayHi();
+
+// напрямую импортируем
+import seyHi from  './js/script';
+sayHi();
